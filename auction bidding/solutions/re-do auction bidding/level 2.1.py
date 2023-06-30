1bde77c5d3a5e1d71791d9e2dcdc0fbed523c021
Bibhuti Bhushan Mukhiya <bibhutibhushanmukhiya4086@gmail.com>
2023-06-30 14:17:16 +0545

    level 2 again completed

--- auction bidding/solutions/re-do auction bidding/level 2.1.py
+++ auction bidding/solutions/re-do auction bidding/level 2.1.py
@@ -2,12 +2,10 @@ def buyerAndbid(bidder_list):
     buyer=bidder_list.pop(0)
     buyerBid=bidder_list.pop(0)
     return buyer,buyerBid
-
 def buyerAndbidRead(bidder_list):
     buyer=bidder_list[0]
     buyerBid=bidder_list[1]
     return buyer,buyerBid
-
 def main(inputVal):
     bidder_list=inputVal.split(',')
     open_price=bidder_list.pop(0)
@@ -16,7 +14,18 @@ def main(inputVal):
     highest_bidder=''
     bidder=''
     bid=''
-    for i in range((len(bidder_list)//2)):
+    buyer=[]
+    for i in range(0,(len(bidder_list)),2):
+        buyer+=[bidder_list[i]]
+    for i in buyer:
+        if bidder_list[0]==i:
+            p=True
+        else:
+            p=False
+            break
+    if p==True:
+        return bidder_list[0],open_price
+    for i in range((len(bidder_list)//2)-1):
         if len(bidder_list)!=0:
             if len(bidder_list)!=0:
                 bidder1,bid1=buyerAndbid(bidder_list)
@@ -35,6 +44,12 @@ def main(inputVal):
                 if int(highest_bid)<int(bid2):
                     bid=highest_bid.replace(highest_bid,str(bid))
                     bidder=highest_bidder.replace(highest_bidder,bidder)
+                if int(highest_bid)>int(bid1) and int(highest_bid)>int(bid2):
+                    bidder=bidder.replace(bidder,highest_bidder)
+                    bid=bid.replace(bid,str(int(bid2)+int(first_bid)))
+                if int(bid1)>int(highest_bid) and int(bid2)>int(highest_bid):
+                    highest_bidder=highest_bidder.replace(highest_bidder,bidder2)
+                    highest_bid=highest_bid.replace(highest_bid,str(int(bid2)+int(first_bid)))
             if int(bid1)>int(bid2)and bidder1!=bidder2:
                 bidder=bidder.replace(bidder,bidder1)
                 bid=bid.replace(bid,str(int(bid2)+int(first_bid)))
@@ -52,15 +67,16 @@ def main(inputVal):
                     bidder=bidder.replace(bidder,bidder2)
                     bid=bid.replace(bid,str(int(highest_bid)+int(first_bid)))
                     highest_bidder=highest_bidder.replace(highest_bidder,bidder)
-                if int(bid1)<int(bid2):    
+                if int(bid1)<int(bid2):
                     bidder=bidder.replace(bidder,bidder1)
                     bid=bid.replace(bid,str(int(bid2)+int(first_bid)))
-                    if int(highest_bid)>int(bid2):
-                        bidder=highest_bidder.replace(highest_bidder,bidder)
                 if int(highest_bid)>int(bid1) and int(highest_bid)>int(bid2):
+                    bid=bid.replace(bid,str(int(bid2)+int(first_bid)))
                     bidder=bidder.replace(bidder,highest_bidder)
-               
+                if int(bid1)>int(highest_bid) and int(bid2)>int(highest_bid):
+                    bid=bid.replace(bid,highest_bid)
+                    bidder=bidder.replace(bidder,highest_bidder)
     return bidder,bid
-inputVal=''''''
+inputVal='''100,C,100,C,115,C,119,C,121,C,144,C,154,C,157,G,158,C,171,C,179,C,194,C,206,C,214,C,227,C,229,C,231,M,300,V,304,'''
 result=main(inputVal)
 print(result[0],result[1])
